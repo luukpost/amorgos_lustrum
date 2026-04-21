e1c22df9e8b9233bde9498e0618a229231ffc50b
luukpost <luuk.post@contentkalender.nl>
2026-04-21 12:25:16 +0200

    feat: add subtitle '2 tot 9 oktober 2027 - Lefkas' to coming soon page

--- src/views/ComingSoon.js
+++ src/views/ComingSoon.js
@@ -6,6 +6,7 @@ const ComingSoon = {
       </div>
       <div class="coming-soon">
         <div class="label">Coming Soon</div>
+        <div class="subtitle">2 tot 9 oktober 2027 &mdash; Lefkas</div>
         <div class="dots">
           <span></span><span></span><span></span>
         </div>
